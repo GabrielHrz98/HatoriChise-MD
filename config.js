8331ac51eefdc1b0eb59cb4d3310bb25da6212e7
GabrielHrz <129259732+GabrielHrz98@users.noreply.github.com>
2023-04-23 22:53:39 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,51 @@
+{
+
+  "ownerNumber": "6285171178013@s.whatsapp.net",
+
+  "ownerName": "GabrielHrz",
+
+  "botName": "PAIN BOT",
+
+  "footer": "gabrielhrz&riycoders",
+
+  "packname": "PAIN BOT",
+
+  "author": "GabrielHrz",
+
+  "pathimg": "./media/pain.jpg",
+
+  "riykey": "gabrielhrz",
+
+  "quoteapi": "https://quote-api.riycoders.my.id/generate",
+
+  "sosmed": {
+
+    "youtube": "https://youtube.com/@gabrielhrz117",
+
+    "instagram": "https://instagram.com/ofcgabrielhrz"
+
+  },
+
+  "donasi": {
+
+    "dana": "085171220913",
+
+    "gopay": "085171220913",
+
+    "pulsa": "085171220913"
+
+  },
+
+  "gamewaktu": 90,
+
+  "limitCount": 25,
+
+  "gcount": {
+
+    "prem": 35,
+
+    "user": 15
+
+  }
+
+}
